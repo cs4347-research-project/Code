23ea7ca47656285c0f7b4b032cf045b32174da3e
Andrew Gonzalez <andrewgonzo23@gmail.com>
2019-04-28 13:28:18 -0500

    most up to date inception model

--- Inception.py
+++ Inception.py
@@ -1,28 +1,6 @@
-import keras
-from keras.layers.core import Layer
-import keras.backend as K
-import tensorflow as tf
-from keras.datasets import cifar10
-from keras.models import Model
-from keras.layers import Conv2D, MaxPool2D, \
-    Dropout, Dense, Input, concatenate, \
-    GlobalAveragePooling2D, AveragePooling2D, \
-    Flatten
-import numpy as np
-from keras import backend as K
-from keras.utils import np_utils
+from __future__ import print_function
 from sklearn.datasets import load_files
-import math
-from keras.optimizers import SGD
-from keras.callbacks import LearningRateScheduler
-
-num_classes = 95
-batch_size = 35
-epochs = 32
-
-train_dir = r"C:\Users\Andrew\Desktop\fruits-360\Training"
-test_dir = r"C:\Users\Andrew\Desktop\fruits-360\Training"
-
+import numpy as np
 
 def load_dataset(path):
     data = load_files(path)
@@ -32,232 +10,114 @@ def load_dataset(path):
     return files, targets, target_labels
 
 
-# training data is just a file name of images. We need to convert them into pixel matrix
-from keras.preprocessing.image import array_to_img, img_to_array, load_img
+train_dir = '/Users/andrewgonzalez/Desktop/fruits-360/Training'
+test_dir = '/Users/andrewgonzalez/Desktop/fruits-360/Test'
+
+num_classes = 95
+batch_size = 35
+epochs = 32
 
 
-def convert_image_to_array(files):
-    images_as_array = []
-    for file in files:
-        # Convert to Numpy Array
-        images_as_array.append(img_to_array(load_img(file)))
-    return images_as_array
-
-
-################################################################################################
-
-x_train, y_train, target_labels = load_dataset(train_dir)
-x_test, y_test, _ = load_dataset(test_dir)
+import keras
+from keras.datasets import cifar10
+#X_train, y_train), (X_test, y_test) = cifar10.load_data()
+X_train, y_train, target_labels = load_dataset(train_dir)
+X_test, y_test, _ = load_dataset(test_dir)
 print('Loading complete!')
 print('Training set size : ', x_train.shape[0])  # 48905
-print('Testing set size: ', x_test.shape[0])  # 16421
-
+print('Testing set size: ', x_test.shape[0])
 no_of_classes = len(np.unique(y_train))
-print(no_of_classes)  # 95 classes
-print(y_train[0:10])
 
-# target labels are numbers of corresponding to class label
-# we need to change them to a vector of 95 elements since
-# we have 95 classes
-from keras.utils import np_utils
 
-y_train = np_utils.to_categorical(y_train, no_of_classes)
-y_test = np_utils.to_categorical(y_test, no_of_classes)
-print(y_train[0])
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
 
-# No we have to divide the validation set into test and validation set
-x_test, x_valid = x_test[7000:], x_test[:7000]
+X_test, x_valid = X_test[7000:], X_test[:7000]
 y_test, y_valid = y_test[7000:], y_test[:7000]
+
 print('Validation X: ', x_valid.shape)
 print('Validation y: ', y_valid.shape)
-print('Test X: ', x_test.shape)
+print('Test X: ', X_test.shape)
 print('Test y: ', y_test.shape)
 
-# Call the converting function to convert the files into array
-
-x_train = np.array(convert_image_to_array(x_train))
-print('Training set shape : ', x_train.shape)
-
+X_train = np.array(convert_image_to_array(X_train))
 x_valid = np.array(convert_image_to_array(x_valid))
+X_test = np.array(convert_image_to_array(X_test))
+print('Training set shape : ', X_train.shape)
 print('Validation set shape : ', x_valid.shape)
-
-x_test = np.array(convert_image_to_array(x_test))
 print('Test set shape : ', x_test.shape)
 
-print('1st training image shape ', x_train[0].shape)
-
-x_train = x_train.astype('float32') / 255
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train = X_train / 255.0
+X_test = X_test / 255.0
 x_valid = x_valid.astype('float32') / 255
-x_test = x_test.astype('float32') / 255
 
 
-def inception_module(x,
-                     filters_1x1,
-                     filters_3x3_reduce,
-                     filters_3x3,
-                     filters_5x5_reduce,
-                     filters_5x5,
-                     filters_pool_proj,
-                     name=None):
-    conv_1x1 = Conv2D(filters_1x1, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init,
-                      bias_initializer=bias_init)(x)
 
-    conv_3x3 = Conv2D(filters_3x3_reduce, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init,
-                      bias_initializer=bias_init)(x)
-    conv_3x3 = Conv2D(filters_3x3, (3, 3), padding='same', activation='relu', kernel_initializer=kernel_init,
-                      bias_initializer=bias_init)(conv_3x3)
+from keras.utils import np_utils
 
-    conv_5x5 = Conv2D(filters_5x5_reduce, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init,
-                      bias_initializer=bias_init)(x)
-    conv_5x5 = Conv2D(filters_5x5, (5, 5), padding='same', activation='relu', kernel_initializer=kernel_init,
-                      bias_initializer=bias_init)(conv_5x5)
+print('Do I get here?')
+from keras.layers import Input
+input_img = Input(shape = (100, 100, 3))
 
-    pool_proj = MaxPool2D((3, 3), strides=(1, 1), padding='same')(x)
-    pool_proj = Conv2D(filters_pool_proj, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init,
-                       bias_initializer=bias_init)(pool_proj)
+from keras.layers import Conv2D, MaxPooling2D
+tower_1 = Conv2D(64, (1,1), padding='same', activation='relu')(input_img)
+tower_1 = Conv2D(64, (3,3), padding='same', activation='relu')(tower_1)
+tower_2 = Conv2D(64, (1,1), padding='same', activation='relu')(input_img)
+tower_2 = Conv2D(64, (5,5), padding='same', activation='relu')(tower_2)
+tower_3 = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_img)
+tower_3 = Conv2D(64, (1,1), padding='same', activation='relu')(tower_3)
 
-    output = concatenate([conv_1x1, conv_3x3, conv_5x5, pool_proj], axis=3, name=name)
+from keras.layers import Conv2D, MaxPooling2D
+tower_1 = Conv2D(64, (1,1), padding='same', activation='relu')(input_img)
+tower_1 = Conv2D(64, (3,3), padding='same', activation='relu')(tower_1)
+tower_2 = Conv2D(64, (1,1), padding='same', activation='relu')(input_img)
+tower_2 = Conv2D(64, (5,5), padding='same', activation='relu')(tower_2)
+tower_3 = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_img)
+tower_3 = Conv2D(64, (1,1), padding='same', activation='relu')(tower_3)
 
-    return output
+output = keras.layers.concatenate([tower_1, tower_2, tower_3], axis = 3)
 
+from keras.layers import Flatten, Dense
+output = Flatten()(output)
+out    = Dense(10, activation='softmax')(output)
 
-kernel_init = keras.initializers.glorot_uniform()
-bias_init = keras.initializers.Constant(value=0.2)
+from keras.models import Model
+model = Model(inputs = input_img, outputs = out)
+# print model.summary()
 
-input_layer = Input(shape=(224, 224, 3))
-
-x = Conv2D(64, (7, 7), padding='same', strides=(2, 2), activation='relu', name='conv_1_7x7/2',
-           kernel_initializer=kernel_init, bias_initializer=bias_init)(input_layer)
-x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_1_3x3/2')(x)
-x = Conv2D(64, (1, 1), padding='same', strides=(1, 1), activation='relu', name='conv_2a_3x3/1')(x)
-x = Conv2D(192, (3, 3), padding='same', strides=(1, 1), activation='relu', name='conv_2b_3x3/1')(x)
-x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_2_3x3/2')(x)
-
-x = inception_module(x,
-                     filters_1x1=64,
-                     filters_3x3_reduce=96,
-                     filters_3x3=128,
-                     filters_5x5_reduce=16,
-                     filters_5x5=32,
-                     filters_pool_proj=32,
-                     name='inception_3a')
-
-x = inception_module(x,
-                     filters_1x1=128,
-                     filters_3x3_reduce=128,
-                     filters_3x3=192,
-                     filters_5x5_reduce=32,
-                     filters_5x5=96,
-                     filters_pool_proj=64,
-                     name='inception_3b')
-
-x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_3_3x3/2')(x)
-
-x = inception_module(x,
-                     filters_1x1=192,
-                     filters_3x3_reduce=96,
-                     filters_3x3=208,
-                     filters_5x5_reduce=16,
-                     filters_5x5=48,
-                     filters_pool_proj=64,
-                     name='inception_4a')
-
-x1 = AveragePooling2D((5, 5), strides=3)(x)
-x1 = Conv2D(128, (1, 1), padding='same', activation='relu')(x1)
-x1 = Flatten()(x1)
-x1 = Dense(1024, activation='relu')(x1)
-x1 = Dropout(0.7)(x1)
-x1 = Dense(10, activation='softmax', name='auxilliary_output_1')(x1)
-
-x = inception_module(x,
-                     filters_1x1=160,
-                     filters_3x3_reduce=112,
-                     filters_3x3=224,
-                     filters_5x5_reduce=24,
-                     filters_5x5=64,
-                     filters_pool_proj=64,
-                     name='inception_4b')
-
-x = inception_module(x,
-                     filters_1x1=128,
-                     filters_3x3_reduce=128,
-                     filters_3x3=256,
-                     filters_5x5_reduce=24,
-                     filters_5x5=64,
-                     filters_pool_proj=64,
-                     name='inception_4c')
-
-x = inception_module(x,
-                     filters_1x1=112,
-                     filters_3x3_reduce=144,
-                     filters_3x3=288,
-                     filters_5x5_reduce=32,
-                     filters_5x5=64,
-                     filters_pool_proj=64,
-                     name='inception_4d')
-
-x2 = AveragePooling2D((5, 5), strides=3)(x)
-x2 = Conv2D(128, (1, 1), padding='same', activation='relu')(x2)
-x2 = Flatten()(x2)
-x2 = Dense(1024, activation='relu')(x2)
-x2 = Dropout(0.7)(x2)
-x2 = Dense(10, activation='softmax', name='auxilliary_output_2')(x2)
-
-x = inception_module(x,
-                     filters_1x1=256,
-                     filters_3x3_reduce=160,
-                     filters_3x3=320,
-                     filters_5x5_reduce=32,
-                     filters_5x5=128,
-                     filters_pool_proj=128,
-                     name='inception_4e')
-
-x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_4_3x3/2')(x)
-
-x = inception_module(x,
-                     filters_1x1=256,
-                     filters_3x3_reduce=160,
-                     filters_3x3=320,
-                     filters_5x5_reduce=32,
-                     filters_5x5=128,
-                     filters_pool_proj=128,
-                     name='inception_5a')
-
-x = inception_module(x,
-                     filters_1x1=384,
-                     filters_3x3_reduce=192,
-                     filters_3x3=384,
-                     filters_5x5_reduce=48,
-                     filters_5x5=128,
-                     filters_pool_proj=128,
-                     name='inception_5b')
-
-x = GlobalAveragePooling2D(name='avg_pool_5_3x3/1')(x)
-
-x = Dropout(0.4)(x)
-
-x = Dense(10, activation='softmax', name='output')(x)
-
-model = Model(input_layer, [x, x1, x2], name='inception_v1')
-
+from keras.optimizers import SGD
 epochs = 25
-initial_lrate = 0.01
+lrate = 0.01
+decay = lrate/epochs
+sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=32)
 
+from keras.models import model_from_json
+import os
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+model.save_weights(os.path.join(os.getcwd(), 'model.h5'))
 
-def decay(epoch, steps=100):
-    initial_lrate = 0.01
-    drop = 0.96
-    epochs_drop = 8
-    lrate = initial_lrate * math.pow(drop, math.floor((1 + epoch) / epochs_drop))
-    return lrate
-
-
-sgd = SGD(lr=initial_lrate, momentum=0.9, nesterov=False)
-
-lr_sc = LearningRateScheduler(decay, verbose=1)
-
-model.compile(loss=['categorical_crossentropy', 'categorical_crossentropy', 'categorical_crossentropy'],
-              loss_weights=[1, 0.3, 0.3], optimizer=sgd, metrics=['accuracy'])
-
-history = model.fit(x_train, [y_train, y_train, y_train], validation_data=(x_test, [y_test, y_test, y_test]),
-                    epochs=epochs, batch_size=256, callbacks=[lr_sc])
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+#Plot the accuracy and loss to help illustrate
+import matplotlib.pyplot as plt
+accuracy = history.history['acc']
+val_accuracy = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(len(accuracy))
+plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
